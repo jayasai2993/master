e2972de0e14aeb29f177701d52b46842816a26eb
jayasai2993 <jayasai@revidd.com>
2025-08-26 22:11:21 +0530

    //Checking for MR

--- app/src/main/java/com/example/ofmen/MainActivity.kt
+++ app/src/main/java/com/example/ofmen/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OFMENTheme {
+                //Checking for MR
                 SplashScreen()
             }
         }
