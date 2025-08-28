98a0631a9db7355f08d3dc793557510779ea41d1
jayasai2993 <jayasai@revidd.com>
2025-08-28 20:03:23 +0530

    Basic HomeScreen

--- app/src/main/java/com/example/ofmen/ui/theme/Type.kt
+++ app/src/main/java/com/example/ofmen/ui/theme/Type.kt
@@ -1,34 +1,32 @@
 package com.example.ofmen.ui.theme
 
+
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.ofmen.R
 
-// Set of Material typography styles to start with
+val BebasNeue = FontFamily(
+    Font(R.font.bebas_neue_regular, FontWeight.Normal)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = BebasNeue,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 32.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = TextStyle(
+        fontFamily = BebasNeue,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default, // fallback for body
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
     )
-    */
 )
